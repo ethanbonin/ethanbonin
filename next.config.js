b0204f1a6dcf99dbd54190d611c8ed01847e2b1f
Christopher Ethan Bonin <ethanbonin@gmail.com>
2021-08-01 15:43:57 -0500

    Added some utils and fixed the relase.yml file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 // eslint-disable-next-line no-undef
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 
 // eslint-disable-next-line no-undef
 module.exports = {
+  // eslint-disable-next-line no-undef
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  // eslint-disable-next-line no-undef
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+
   reactStrictMode: true,
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/ethanbonin/ethanbonin.github.io/gh-pages/' : '',
+  // // eslint-disable-next-line no-dupe-keys
+  // assetPrefix: isProd ? 'https://cdn.statically.io/gh/ethanbonin/ethanbonin.github.io/gh-pages/' : '',
 }
---
+++ utils.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-undef
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
+export { prefix }
+
+
+
+// EXAMPLE To fix the non-loading images on NEXT.js
+// ...
+// import { prefix } from '../../utils/prefix.js';
+// ...
+// <img src={`${prefix}/someimage.jpg`} alt='some alt text' />
+// ...
