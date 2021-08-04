2b8a60dbf113dca35787be9cd5ee1e3cf71e97aa
Christopher Ethan Bonin <ethanbonin@gmail.com>
2021-08-04 11:14:14 -0500

    Fixed an es-lint rule

--- pages/routes/index.js
+++ pages/routes/index.js
@@ -1,9 +1,11 @@
 // eslint-disable-next-line no-undef
 const prefix = process.env.NEXT_PUBLIC_BASE_PATH ?? ""
 
-export default {
+const routes = {
     index: prefix + "/",
     about: prefix + "/about",
     projects: prefix +"/projects",
     history: prefix + "/history"
 }
+
+export default routes
